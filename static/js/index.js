90e391e196718c0c9f5e4e394480e76cd88bdbf7
YJ <zerosix1017@gmail.com>
2023-05-26 00:18:33 +0900

    feat: 채팅방 ui 구현

--- static/js/index.js
+++ static/js/index.js
@@ -13,7 +13,29 @@ socket.on('connect', function() {
 })
 
 socket.on('update', function(data) {
-    console.log('${data.name}: ${data.messasge}')
+    var chat = document.getElementById('chat')
+    var message = document.createElement('div')
+    var node = document.createTextNode(`${data.name}: ${data.message}`)
+    var className = ''
+
+    // 타입에 따라 적용하는 클래스 다르게 지정
+    switch(data.type) {
+        case 'message':
+            className = 'other'
+            break
+        
+        case 'connect':
+            className = 'connect'
+            break
+        
+        case 'disconnect':
+            className = 'disconnect'
+            break
+    }
+
+    message.classList.add(className)
+    message.appendChild(node)
+    chat.appendChild(message)
 })
 
 // 전송 function
@@ -23,6 +45,14 @@ function send() {
 
     // 가져온 후에는 test를 빈칸으로 바꿈
     document.getElementById('test').value = ''
+
+    // 내가 전송할 메세지 표시
+    var chat = document.getElementById('chat')
+    var msg = document.createElement('div')
+    var node = document.createTextNode(message)
+    msg.classList.add('me')
+    msg.appendChild(node)
+    chat.appendChild(msg)
 
     // 서버로 메세지 send
     socket.emit('message', {type: 'message', message: message})
